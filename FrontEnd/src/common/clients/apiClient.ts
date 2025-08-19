b336fefecd278b7daaf7a1021b12611fc232982f
hazemAzzam <hazemmohamed9194@gmail.com>
2025-08-19 13:44:51 +0300

    refresh token

--- FrontEnd/src/common/clients/apiClient.ts
+++ FrontEnd/src/common/clients/apiClient.ts
@@ -35,7 +35,7 @@ apiClient.interceptors.response.use(
       try {
         const refreshToken = useAccessRefreshTokenStore.getState().refresh;
 
-        const response = await axios.post(`http://127.0.0.1/api${ENDPOINTS.AUTH.REFRESH}/`, { refresh: refreshToken }, { withCredentials: true });
+        const response = await axios.post(`http://127.0.0.1:8000/api${ENDPOINTS.AUTH.REFRESH}/`, { refresh: refreshToken }, { withCredentials: true });
 
         const newAccessToken = response.data.access;
 
